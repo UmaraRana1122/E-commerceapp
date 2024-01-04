5e566f6d2c17417265370fc529642c5e7778d298
umararana1122 <umara.rana@techbolic.com>
2024-01-04 17:46:58 +0500

    Upadate cart screen

--- lib/E-commerceApp/CartScreen.dart
+++ lib/E-commerceApp/CartScreen.dart
@@ -4,6 +4,6 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(body: Column(children: [],),);
   }
 }
